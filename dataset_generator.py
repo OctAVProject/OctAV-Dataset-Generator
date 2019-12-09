f12679eec87458c139e1c9918f7cdb9bfb45fe6f
ShellCode33 <shellcode33@protonmail.ch>
2019-12-09 21:22:00 +0100

    Add some output between binaries

--- dataset_generator.py
+++ dataset_generator.py
@@ -17,16 +17,15 @@ def generate_legit_binaries_dataset():
 
     # TODO with multiprocessing.Pool(processes=4) as pool:  ??
 
-    # analyse_legit_binary("/bin/ls")
-    # analyse_legit_binary("/bin/gzip")
-
     for bin_dir in LEGIT_BINARIES_LOCATIONS:
         files = os.listdir(bin_dir)
+        print(f"Executing {len(files)} binaries from '{bin_dir}'...\n")
 
         for file in files:
             full_path = bin_dir + "/" + file
             if os.path.isfile(full_path):
                 analyse_legit_binary(full_path)
+                print()  # seperation between each binary
 
 
 def generate_malwares_dataset():
